a455ddde4e3aa3d0814d82f6830f2e02ef3e63d2
Davd Phillips <daphillips19@gmail.com>
2023-12-05 23:35:01 -0500

    through day 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,29 +4,29 @@ import java.util.*
 plugins {
     idea
 //    alias(libs.plugins.gradle.versions)
- id("com.github.ben-manes.versions") version "0.50.0"
+    id("com.github.ben-manes.versions") version "0.50.0"
 }
 
 // checks to see if a release is "stable," meaning we don't have to worry about rc versions polluting a dependencyUpdates check
 // this can be found in the versions plugin readme (https://github.com/ben-manes/gradle-versions-plugin)
 fun isNonStable(version: String): Boolean {
- val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase(Locale.getDefault()).contains(it) }
- val regex = "^[0-9,.v-]+(-r)?$".toRegex()
- val isStable = stableKeyword || regex.matches(version)
- return isStable.not()
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase(Locale.getDefault()).contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
 }
 
 tasks.named("dependencyUpdates", DependencyUpdatesTask::class.java).configure {
- // disallow release candidates as upgradable versions from stable versions
- rejectVersionIf {
-  isNonStable(candidate.version) && !isNonStable(currentVersion)
- }
- 
- // don't look at gradle rc versions either
- gradleReleaseChannel = "current"
+    // disallow release candidates as upgradable versions from stable versions
+    rejectVersionIf {
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
+    }
+
+    // don't look at gradle rc versions either
+    gradleReleaseChannel = "current"
 }
 
 // https://youtrack.jetbrains.com/issue/KT-48410/Sync-failed.-Could-not-determine-the-dependencies-of-task-commonizeNativeDistribution.#focus=Comments-27-5144160.0-0
 repositories {
- mavenCentral()
+    mavenCentral()
 }
